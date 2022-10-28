306cd67a47f8c4bc50567314a3b3e6ae912c27e2
MalikMFasih <114776307+MalikMFasih@users.noreply.github.com>
2022-10-28 15:40:38 +0500

    Add files via upload

---
+++ Machine Learning/Question1.py
@@ -0,0 +1,11 @@
+# Exercise no.5.3
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
